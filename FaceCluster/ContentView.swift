51ce59efd094a3f59392aa522649e3773662b033
El-Mundo <superbluecat71@outlook.com>
2024-06-01 09:45:44 +0100

    Initial Commit

---
+++ FaceCluster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceCluster
+//
+//  Created by 曹双源 on 01/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceCluster/FaceClusterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceClusterApp.swift
+//  FaceCluster
+//
+//  Created by 曹双源 on 01/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceClusterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
